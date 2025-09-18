c30044fef9455004f350c868de8d63a2c38a979f
makondakhchan <makondakhchan@hex.team>
2025-09-18 09:33:39 +0300

    add some bullshit

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -61,6 +61,61 @@ require('lazy').setup({
       vim.g.vimtex_view_method = 'zathura'
     end,
   },
+  {
+    'lewis6991/hover.nvim',
+    lazy = true,
+    specs = {
+      {
+        'AstroNvim/astrocore',
+        opts = {
+          options = { opt = { mousemoveevent = true } },
+          mappings = {
+            n = {
+              K = {
+                function()
+                  require('hover').hover()
+                end,
+                desc = 'Hover cursor',
+              },
+              gK = {
+                function()
+                  require('hover').hover_select()
+                end,
+                desc = 'Hover selection',
+              },
+              [']h'] = {
+                function()
+                  require('hover').hover_switch 'next'
+                end,
+                desc = 'Next hover source',
+              },
+              ['[h'] = {
+                function()
+                  require('hover').hover_switch 'previous'
+                end,
+                desc = 'Previous hover source',
+              },
+              ['<MouseMove>'] = {
+                function()
+                  require('hover').hover_mouse()
+                end,
+                desc = 'Hover mouse',
+              },
+            },
+          },
+        },
+      },
+      {
+        'AstroNvim/astrolsp',
+        opts = { mappings = { n = { K = false } } },
+      },
+    },
+    opts = {
+      init = function()
+        require 'hover.providers.lsp'
+      end,
+    },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
