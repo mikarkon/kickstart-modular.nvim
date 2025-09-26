5f1100d073a037c3be5b8df89bc3bd3196e9efcf
makondakhchan <makondakhchan@hex.team>
2025-09-26 15:39:02 +0300

    add undotree and treesitter

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -45,4 +45,11 @@ return {
       }
     end,
   },
+  {
+    'mbbill/undotree',
+    lazy = false, -- load on startup or set to true with cmd lazy load
+    config = function()
+      vim.api.nvim_set_keymap('n', '<leader>u', '<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>', { noremap = true, silent = true })
+    end,
+  },
 }
